775d6dcddfed7bfa39880915c15fd932fc11440e
Eliud Githuku <githukueliud@gmail.com>
2023-10-06 23:22:05 +0300

    Onboarding screens done

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnboardingScreen.kt
@@ -1,15 +1,32 @@
 package com.loc.newsapp.presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.loc.newsapp.presentation.common.NewsButton
+import com.loc.newsapp.presentation.common.NewsTextButton
+import com.loc.newsapp.presentation.onboarding.Dimens.MediumPadding1
+import com.loc.newsapp.presentation.onboarding.Dimens.PageIndicatorWidth
 import com.loc.newsapp.presentation.onboarding.components.OnboardingPage
+import com.loc.newsapp.presentation.onboarding.components.PageIndicator
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -17,7 +34,9 @@ fun OnboardingScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
     ) {
         val pageState = rememberPagerState(initialPage = 0) {
             pages.size
@@ -27,7 +46,7 @@ fun OnboardingScreen(
                 when(pageState.currentPage) {
                     0 -> listOf("", "Next")
                     1 -> listOf("Back", "Next")
-                    2 -> listOf("Back", "Next")
+                    2 -> listOf("Back", "Get Started")
                     else -> listOf("", "")
                 }
             }
@@ -35,5 +54,53 @@ fun OnboardingScreen(
         HorizontalPager(state = pageState) {index ->
             OnboardingPage(page = pages[index])
         }
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = MediumPadding1)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PageIndicator(
+                modifier = Modifier.width(PageIndicatorWidth),
+                pageSize = pages.size,
+                selectedPage = pageState.currentPage
+            )
+
+
+            Row(
+                modifier = Modifier,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val scope = rememberCoroutineScope()
+                if (buttonState.value[0].isNotEmpty()) {
+                    NewsTextButton(
+                        text = buttonState.value[0],
+                        onClick = {
+                            scope.launch {
+                                pageState.animateScrollToPage(page = pageState.currentPage - 1)
+                            }
+                        }
+                    )
+                }
+                NewsButton(
+                    text = buttonState.value[1],
+                    onClick = {
+                        scope.launch {
+                            if (pageState.currentPage == 3) {
+                                //To do navigate to home screen
+                            } else {
+                                pageState.animateScrollToPage(page = pageState.currentPage + 1)
+                            }
+                        }
+                    }
+                )
+
+            }
+
+        }
+        Spacer(modifier = Modifier.weight(0.5f)) //placed after the column
     }
 }
