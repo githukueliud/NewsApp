61ba5e77a41d31dc9095cd6e3f2de7ec04f8b88b
Eliud Githuku <githukueliud@gmail.com>
2023-10-08 13:57:10 +0300

    Shimmer effect created

--- app/src/main/java/com/loc/newsapp/presentation/Dimens.kt
+++ app/src/main/java/com/loc/newsapp/presentation/Dimens.kt
@@ -8,4 +8,7 @@ object Dimens {
     val PageIndicatorWidth = 52.dp
     val IndicatorSize = 14.dp
     val ArticleCardSize = 96.dp
+    val ExtraSmallPadding = 3.dp
+    val ExtraSmallPadding2 = 6.dp
+    val SmallIconSize = 11.dp
 }
